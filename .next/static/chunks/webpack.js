351b33721d0269483ee239edbf867b1ee6023397
kotu-kotuo <ttn022300@gmail.com>
2022-08-27 13:20:54 +0900

    🔧 コンフィグ追加

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5ee6c81e0cc70bf2"; }
+/******/ 		__webpack_require__.h = function() { return "d4350a9055c73c68"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
-import { NextPage } from "next";
+import { GetStaticProps, NextPage } from "next";
 import Layout from "@/components/common/Layout";
 import Card from "@/components/common/Card";
 import Container from "@/components/common/parts/Container";
+import { fetchArticles } from "@/lib/notion";
 
-const Home: NextPage = () => {
+const Home: NextPage = (props) => {
+  console.log(props);
   return (
     <Layout path="/" title="NonoTech" noTitleTemplate isTopPage>
       <Container>
@@ -15,6 +17,11 @@ const Home: NextPage = () => {
       </Container>
     </Layout>
   );
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  const data = await fetchArticles();
+  return { props: data ? data : [], revalidate: 10 };
 };
 
 export default Home;
