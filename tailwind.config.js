e6f2af7155bfd54163d04a0c1ef78ca37530491e
kotu-kotuo <ttn022300@gmail.com>
2022-09-25 15:45:49 +0900

    フォント変更

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,22 @@ module.exports = {
     "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    fontFamily: {},
+    fontFamily: {
+      sans: [
+        "Helvetica Neue",
+        "Arial",
+        "system-ui",
+        "-apple-system",
+        "BlinkMacSystemFont",
+        "Noto Sans JP",
+        "Segoe UI",
+        "sans-serif",
+        "Apple Color Emoji",
+        "Segoe UI Emoji",
+        "Segoe UI Symbol",
+        "Noto Color Emoji",
+      ],
+    },
     extend: {
       colors: {
         primary: {
