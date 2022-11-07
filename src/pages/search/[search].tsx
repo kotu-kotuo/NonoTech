18d5fe2fd56a4062ec472d92f2c6229840fce750
Nono <52486487+kotu-kotuo@users.noreply.github.com>
2022-11-07 18:04:27 +0900

    記事が見つかりませんでした、を作成

--- src/pages/search/[search].tsx
+++ src/pages/search/[search].tsx
@@ -3,6 +3,8 @@ import Layout from "@/components/layout/Layout";
 import { fetchDatabase, searchPages } from "@/lib/notion";
 import { IndexProps, Params } from "@/types/types";
 import { GetServerSideProps, NextPage } from "next";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const Search: NextPage<IndexProps> = ({ pages, database }) => {
@@ -12,6 +14,24 @@ const Search: NextPage<IndexProps> = ({ pages, database }) => {
         {pages.map((page, index) => (
           <Card key={index} page={page} />
         ))}
+        {pages.length === 0 ? (
+          <div>
+            <div className="py-8 text-center text-3xl font-bold text-gray-600 ">
+              記事が見つかりませんでした。。
+            </div>
+            <div className="relative">
+              <Image width={700} height={400} src="/sika.jpg" />
+              <p className="absolute top-1/2 right-1/4 font-extralight text-white">
+                まじごめん
+              </p>
+            </div>
+            <Link href={"/"}>
+              <p className="mt-6 inline-block cursor-pointer border-b border-secondary leading-tight text-secondary transition-colors duration-200 hover:border-b-primary hover:text-primary">
+                ホームへ戻る
+              </p>
+            </Link>
+          </div>
+        ) : null}
       </div>
     </Layout>
   );
