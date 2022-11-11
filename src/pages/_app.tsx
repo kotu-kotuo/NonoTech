bcfd5b7241ad906688f320cd4eb99ff1c675c976
Nono <52486487+kotu-kotuo@users.noreply.github.com>
2022-11-11 17:29:43 +0900

    PV計測のhooks作成

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,8 +4,10 @@ import "react-notion-x/src/styles.css";
 // used for code syntax highlighting (optional)
 import "prismjs/themes/prism-tomorrow.css";
 import type { AppProps } from "next/app";
+import { usePageView } from "@/hooks/usePageView";
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+  usePageView();
   return <Component {...pageProps} />;
 }
 export default MyApp;
